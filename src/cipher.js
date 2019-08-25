03fe40ddcb42b39f6746fc584c611b42c347c5ae
crusiris <cruzsirys@gmail.com>
2019-08-24 20:06:10 -0400

    Html, Javascript

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,68 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+    encode: (offset, mensaje) => {
+
+        let finalArray = " ";
+        let arrayLetter = [];
+
+
+        for (var i = 0; i < mensaje.length; i++) {
+
+
+            //Obteniendo valor de la letra en ascii usando la formula.
+            letter = mensaje.charCodeAt(i);
+
+            console.log(letter);
+
+            if (letter != 32) {
+                //aplicando formula
+                nAscii = (letter - 65 + offset) % 26 + 65;
+
+                // letra alfabetica
+                labcdario = String.fromCharCode(nAscii);
+
+                //guardo en un array
+                arrayLetter.push(labcdario);
+
+            } else if (letter = 32) {
+                letter = " ";
+                arrayLetter.push(letter);
+            }
+
+        }
+        //De array a cadena
+        finalArray = arrayLetter.join("");
+
+        return finalArray;
+    },
+
+    decode: (offsett, mensajee) => {
+        let finalArrayD = " ";
+        let arrayLetterD = [];
+
+        for (let j = 0; j < mensajee.length; j++) {
+
+            let letterD = mensajee.charCodeAt(j);
+
+
+            if (letterD != 32) {
+
+                cesarAscii = (letterD + 65 + (-offsett)) % 26 + 65;
+
+                cesarAlfa = String.fromCharCode(cesarAscii);
+
+                arrayLetterD.push(cesarAlfa);
+
+            } else if (letterD = 32) {
+                letterD = " ";
+                arrayLetterD.push(letterD);
+            }
+
+
+
+
+
+        }
+        finalArrayD = arrayLetterD.join("");
+        return finalArrayD;
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-/*Acá va tu código*/
+//Boton que cambia a la pantalla donde se visualizan los mensaje(funcion screenMsj)
+
+document.getElementById("btnEncryptScreen").addEventListener("click", () => {
+    document.getElementById("container-main").innerHTML = " ";
+    document.getElementById("container-maintwo").style.display = "block";
+});
+
+document.getElementById("btnDecryptScreen").addEventListener("click", () => {
+    document.getElementById("container-main").innerHTML = " ";
+    document.getElementById("container-mainthree").style.display = "block";
+
+});
+
+//Boton de cifrado (obtengo el valor y mando la informacion al cipher.js) (funncion btncipher)
+
+document.getElementById("btnencrypt").addEventListener("click", () => {
+    let offset = parseInt(document.getElementById("position").value);
+    console.log(offset);
+    let mensaje = document.getElementById("msjcipher").value.toUpperCase();
+    console.log(mensaje);
+    // document.getElementById("container-maintwo").style.display = "block";
+
+    let msjtrad = cipher.encode(offset, mensaje);
+    document.getElementById("msjdecipher").innerHTML = msjtrad;
+
+});
+
+//Boton de descifrado 
+document.getElementById("btndecrypt").addEventListener("click", () => {
+    let offsett = parseInt(document.getElementById("offsett").value);
+    let mensajee = document.getElementById("msjedecipher").value.toUpperCase();
+
+    //asigno a una variable el valor el return de la funcion
+    let msjtraduc = cipher.decode(offsett, mensajee);
+    console.log(msjtraduc);
+    //imprimo en pantalla el resultado de mi funcion
+    document.getElementById("msjcipherr").innerHTML = msjtraduc;
+})
